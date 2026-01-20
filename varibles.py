37f0580a52b3aa57c7605ac6f501496e74c28eda
Aisha <aishafatima000007@gmail.com>
2026-01-20 10:54:22 +0000

    Added variables practice

---
+++ varibles.py
@@ -0,0 +1,36 @@
+print("hello world")
+print("my name is aisha","my age is 21")
+print(55+99)
+
+#VARIABLES
+#A variable is a name given to a memory location in a program.
+#name = "aisha" #string(" ")
+#age = 21
+#price = 25.99
+#print(name)
+# name = "aisha"
+# print("my name is :", name)
+# age = 21
+# age2 = age
+# print(age2)
+
+# name = "aisha"
+# print(type(name))
+
+# age = 21
+# print(type(age))
+
+# price = 28.99
+# print(type(price))
+
+# old = False
+# print(type(old))
+
+# a = None
+# print(type(a))
+
+
+
+
+
+

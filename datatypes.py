6fb7c83f362965ae5e390d8b74f02337a2717a0b
Aisha <aishafatima000007@gmail.com>
2026-01-25 16:00:27 +0000

    datatypes

---
+++ datatypes.py
@@ -0,0 +1,19 @@
+#DATA_TYPES
+#(Integers,string,float,boolean(T/F),none)
+
+#KEYWORDS
+#(Keywords are the reserved words in python)
+#eg; and,as,else,in,is,lamda,with,return,from,global,def,del,elif...
+#we cannot use the reserve words as variables in python
+
+#CaseSensitive 
+#(python is case sensitive means A not equal to a)
+
+print sum
+a = 5
+b = 2
+sum = a + b
+print(sum)
+ 
+#comments (not execute in program) 
+#shortcut key to comment (Ctrl+/)

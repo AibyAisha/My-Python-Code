f24d68dcbc0fd1a9cc8bf46f19fd511b2b592f45
Aisha <aishafatima000007@gmail.com>
2026-01-26 16:00:44 +0000

    operators

---
+++ operators.py
@@ -0,0 +1,14 @@
+#Types of Operators
+#1)Arithmetic operators = +,-,*,/,%,**    (if we divide any no. the output will be always in floating val)
+# % = to find remainder  ** = a^b
+
+#2)Relational/Comparision Operators= (==,!=,>,<,>=,<=)  != (not equal to)
+
+#3)Assignment Operators (=, +=, -=, *=, /=, %=, **= )
+
+#4)Logical Operators (not,and,or)
+print(not False)
+val1 = False
+val2 = True
+print("and operator : " , val1 and val2)
+print("and operator : " , val1 or val2)

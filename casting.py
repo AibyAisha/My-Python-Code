6be525d1d18a04d1d7ddc1496f21347726439a77
Aisha <aishafatima000007@gmail.com>
2026-01-27 07:26:04 +0000

    casting

---
+++ casting.py
@@ -0,0 +1,13 @@
+#Type_Conversion (automatic)
+a = 2
+b = 4.25
+sum = a + b
+print(type(sum))
+# float>>>int float...
+
+#Type_Casting (manually)
+# int(val)
+# float(val)
+
+# val = float("5")
+# print(type(val))
